bda468ce76348e3e699def0516fe74e4f0058dc8
mainug <alsdnr0850@gmail.com>
2026-05-21 18:16:19 +0900

    spring boot 전 굽기 완료

---
+++ back/src/main/java/com/toyproject/back/entity/TravelEntry.java
@@ -0,0 +1,44 @@
+package com.toyproject.back.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class TravelEntry {
+
+    @Id
+    @SequenceGenerator(
+            name = "travel_seq_generator",
+            sequenceName = "travel_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "travel_seq_generator"
+    )
+    private Long id;
+
+    private String title;
+
+    private String locationName;
+
+    private LocalDate date;
+
+    @Column(length = 2000)
+    private String content;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private Integer visits;
+
+    private String imageUrl;
+
+    private String region;
+}
